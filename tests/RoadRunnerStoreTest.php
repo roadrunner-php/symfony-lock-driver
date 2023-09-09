1e7276f62a2d8b9cd9f69c1f05366b36e8876f1d
gam6itko <gam6itko@gmail.com>
2023-09-09 14:37:39 +0300

    up

--- tests/RoadRunnerStoreTest.php
+++ tests/RoadRunnerStoreTest.php
@@ -16,7 +16,7 @@ final class RoadRunnerStoreTest extends TestCase
         $rrLock = $this->createMock(RrLock::class);
         $rrLock->expects(self::once())
             ->method('lock')
-            ->with('resource-name');
+            ->with('resource-name', '*');
         $store = new RoadRunnerStore($rrLock);
         $store->save(new Key('resource-name'));
     }
@@ -26,7 +26,7 @@ final class RoadRunnerStoreTest extends TestCase
         $rrLock = $this->createMock(RrLock::class);
         $rrLock->expects(self::once())
             ->method('exists')
-            ->with('resource-name');
+            ->with('resource-name', '*');
         $store = new RoadRunnerStore($rrLock);
         $store->exists(new Key('resource-name'));
     }
@@ -39,5 +39,25 @@ final class RoadRunnerStoreTest extends TestCase
             ->with('resource-name', '*', 3600.0);
         $store = new RoadRunnerStore($rrLock);
         $store->putOffExpiration(new Key('resource-name'), 3600.0);
+    }
+
+    public function testDelete(): void
+    {
+        $rrLock = $this->createMock(RrLock::class);
+        $rrLock->expects(self::once())
+            ->method('forceRelease')
+            ->with('resource-name');
+        $store = new RoadRunnerStore($rrLock);
+        $store->delete(new Key('resource-name'));
+    }
+
+    public function testSaveRead(): void
+    {
+        $rrLock = $this->createMock(RrLock::class);
+        $rrLock->expects(self::once())
+            ->method('lockRead')
+            ->with('resource-name', '*');
+        $store = new RoadRunnerStore($rrLock);
+        $store->saveRead(new Key('resource-name'));
     }
 }
